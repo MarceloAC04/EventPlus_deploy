2f7794ea76a0606f5cac41df2cc0977fadd5e9a8
Marcelo <marcelocardoso020304@gmail.com>
2023-12-13 17:03:33 -0300

    ajustes

--- eventplus/src/pages/DetalhesEventosPage/DetalhesEventosPage.jsx
+++ eventplus/src/pages/DetalhesEventosPage/DetalhesEventosPage.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
 import MainContent from "../../componentes/MainContent/MainContent";
+import useParams from 'react-router-dom';
 import Titulo from "../../componentes/Titulo/Titulo";
 import Container from "../../componentes/Container/Container";
 import TableDe from "./TableDe/TableDe";
 import { useContext } from "react";
 import { UserContex } from "../../context/AuthContext";
-import api, { eventResource } from "../../Services/Service";
+import api, { commentaryEventResource, eventResource } from "../../Services/Service";
 import "./DetalhesEventosPage.css";
 
 const DetalhesEventosPage = () => {
   const { userData, setUserData } = useContext(UserContex);
+  const {idEvento, nomeEvento} = useParams()
 
   const [eventos, setEventos] = useState([]);
+  const [comentarios, setComentarios] = useState([]);
 
   async function loadEvents() {
     try {
@@ -20,6 +23,18 @@ const DetalhesEventosPage = () => {
       console.log(retorno);
     } catch (error) {
       console.log("Erro na api");
+      console.log(error);
+    }
+  }
+
+  async function getComentarios() {
+    try {
+      const route = userData.role == 'Aluno' ? 'Listar' : 'ListarSomenteExibe';
+      const promise = await api.get(`${commentaryEventResource}/${route}/${idEvento}`);
+      const dados = promise.data;
+
+      setComentarios(dados);
+    } catch (error) {
       console.log(error);
     }
   }
